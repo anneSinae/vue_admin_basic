1c5aa68c7d9b52d56d732d40a842bdbc3f1018c1
Anne.Sinae Yu <anne.sinae@gmail.com>
2022-04-14 14:29:43 +0900

    목록 detail 모달로 보여주기(props, emit)

---
+++ src/components/DetailModal.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="modal-wrap">
+        <div class="modal">
+            <div class="detail">
+                <h1>목록상세페이지</h1>
+                <p>{{ detailData.email }}</p>
+                <p>{{ detailData.name }}</p>
+                <p>{{ detailData.phoneNumber }}</p>
+            </div>
+            <el-button @click="$emit('close-modal')">닫기</el-button>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'DetailModal-component',
+
+    props: {
+        detailData: {},
+    },
+
+    mounted() {
+        console.log(this.detailData, "sinae");
+    }
+}
+</script>
--- src/views/manager/List.vue
+++ src/views/manager/List.vue
@@ -3,7 +3,7 @@
     <DateRange />
 
     <ul class="admin-list">
-        <li v-for="item in adminList" :key="item">
+        <li v-for="item in adminList" :key="item" @click="rowClicked(item)">
             <p>{{item.email}}</p>
             <p>{{item.name}}</p>
             <p>{{item.phoneNumber}}</p>
@@ -14,21 +14,31 @@
         </li>
     </ul>
     <div class="total">전체: {{adminListTotal}}</div>
+
+    <DetailModal 
+        v-if="showDetail" 
+        @close-modal="showDetail=false" 
+        class="fade-in"
+        :detailData="itemData"
+     />
 </template>
 
 <script>
 import apiManagers from '@/api/manager'
 import DateRange from '@/components/DateRange.vue'
+import DetailModal from '@/components/DetailModal.vue'
 
 export default {
     name: 'List-page',
     
-    components: {DateRange},
+    components: {DateRange, DetailModal},
 
     data() {
         return {
             adminList: null,
             adminListTotal: 0,
+            showDetail: false,
+            itemData: null,
         }
     },
     
@@ -44,6 +54,14 @@ export default {
                 console.log(e);
             });
     },
+
+    methods: {
+        rowClicked(data) {
+            this.showDetail = true;
+            this.itemData = data;
+            console.log(data, "test")
+        },
+    }
 
 };
 </script>
