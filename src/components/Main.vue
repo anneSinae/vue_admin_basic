0c9ac5c182eacbb20614e0d5b8757f4faa22a1ab
Anne.Sinae Yu <anne.sinae@gmail.com>
2022-05-11 19:07:21 +0900

    전역변수 추가(같은 store파일)

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,10 @@
 <template>
     <h3 class="title">Main</h3>
+    <div style="margin: 15px 0; padding: 15px; border: 1px solid #ddd;">
+        {{ this.$store.getters.mySample }}<br>
+        <button @click="resetSample">전역변수 sample 업데이트</button>
+        <button @click="removeSample">전역변수 sample 데이터삭제</button>
+    </div>
     <Modal>
         <SampleModal />
     </Modal>
@@ -8,12 +13,21 @@
 <script>
 import Modal from '../components/Modal.vue'
 import SampleModal from '../components/SampleModal.vue'
+import store from '@/store'
 
 export default {
     name: 'Main-page',
     components: { 
         Modal, 
         SampleModal 
+    },
+    methods: {
+        resetSample() {
+            store.dispatch("updateSample", "my sample(global data) update");
+        },
+        removeSample() {
+            store.dispatch("updateSample", "삭제됨");
+        }
     }
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -4,20 +4,30 @@ import createPersistedState from 'vuex-persistedstate'
 export default createStore({
   state: {
     token: "",
+    sample: "my sample",
   },
   getters: {
     isLogined: (state) => {
       return state.token !== "";
+    },
+    mySample: (state) => {
+      return state.sample;
     } 
   },
   actions: {
     updateToken({commit}, token) {
       commit("UPDATE_TOKEN", token);
+    },
+    updateSample({commit}, sample) {
+      commit("UPDATE_SAMPLE", sample);
     }
   },
   mutations: {
     UPDATE_TOKEN(state, token) {
       state.token = token;
+    },
+    UPDATE_SAMPLE(state, sample) {
+      state.sample = sample;
     }
   },
   modules: {
